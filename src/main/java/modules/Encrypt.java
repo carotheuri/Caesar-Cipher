151c230f1d1d337da74cddad1960dba9c998177b
carotheuri <princesstheush2@gmail.com>
2023-07-27 15:05:49 +0300

    creating a hashmap to translate few alphabet letters to numbers

--- src/main/java/modules/Encrypt.java
+++ src/main/java/modules/Encrypt.java
@@ -1,10 +1,18 @@
 package modules;
 
+import java.util.HashMap;
+
 class Encrypt {
     private int key;
     private String word;
     public Encrypt(int shiftKey, String wordToDecrypt) {
-
+        HashMap<String,Integer> hmp = new HashMap<String,Integer>();
+        hmp.put("A",1);
+        hmp.put("B",2);
+        hmp.put("C",3);
+        hmp.put("D",4);
+        hmp.put("E",5);
+        hmp.put("F",6);
     }
     public static String encryptSentence(){
         String newWord = "QEB";
