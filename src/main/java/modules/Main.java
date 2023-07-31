63e7d7d24f06061aabd2211ea5483d48cf372635
carotheuri <princesstheush2@gmail.com>
2023-07-31 11:35:47 +0300

    creating decrypt user input

--- src/main/java/modules/Main.java
+++ src/main/java/modules/Main.java
@@ -8,6 +8,7 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         boolean programRunning = true;
+        String output = "";
         while(programRunning){
             System.out.println("What would you like to do?. To encrypt put 1 to decrypt put 2. To exit type exit.");
             String navigationChosen = bufferedReader.readLine();
@@ -16,9 +17,17 @@ public class Main {
                 System.out.println("Type the word you would like to encrypt");
                 String wordToEncrypt = bufferedReader.readLine();
                 Encrypt word = new Encrypt(3,wordToEncrypt);
-                String sentenceOne = String.format("The encrypted word is: %s", word.encryptSentence());
-                System.out.println(sentenceOne);
-            } else if (navigationChosen.equals("exit")) {
+                output = String.format("The encrypted word is: %s", word.encryptSentence());
+                System.out.println(output);
+            }
+            else if(navigationChosen.equals("2")) {
+                System.out.println("Type the word you would like to decrypt");
+                String wordToDecrypt = bufferedReader.readLine();
+                Decrypt word = new Decrypt(3, wordToDecrypt);
+                output = String.format("The decrypted word is: %s", word.decryptSentence());
+                System.out.println(output);
+            }
+            else if (navigationChosen.equals("exit")) {
                 programRunning = false;
             }
             else{
