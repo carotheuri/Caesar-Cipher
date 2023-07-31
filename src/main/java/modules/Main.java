77581e912a6cb84521b3ae082aa507b242feadf2
carotheuri <princesstheush2@gmail.com>
2023-07-31 12:12:28 +0300

    refactor code to use getter method for get word when getting the original string fro encryption while decrypting

--- src/main/java/modules/Main.java
+++ src/main/java/modules/Main.java
@@ -9,21 +9,23 @@ public class Main {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         boolean programRunning = true;
         String output = "";
-        String wordToEncrypt = "";
+        String originalWord = "";
+
         while(programRunning){
             System.out.println("What would you like to do?. To encrypt put 1 to decrypt put 2. To exit type exit.");
             String navigationChosen = bufferedReader.readLine();
 
             if(navigationChosen.equals("1")){
                 System.out.println("Type the word you would like to encrypt");
-                wordToEncrypt = bufferedReader.readLine();
+                String wordToEncrypt = bufferedReader.readLine();
                 Encrypt word = new Encrypt(3,wordToEncrypt);
+                originalWord = word.getWord();
                 output = String.format("The encrypted string is: %s", word.encryptSentence());
                 System.out.println(output);
             }
             else if(navigationChosen.equals("2")) {
-                if(!wordToEncrypt.equals("")){
-                    output = String.format("The decrypted string is: %s", wordToEncrypt);
+                if(!originalWord.equals("")){
+                    output = String.format("The decrypted string is: %s",originalWord);
                     System.out.println(output);
                 }
                 else{
